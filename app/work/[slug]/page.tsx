07367cf40898471be8e6318305117d9a48c66315
Andrew <aj.quesada1990@gmail.com>
2025-05-13 14:51:41 -0400

    setting up de inital structure

---
+++ app/work/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Work = () => {
+  return <div>this is the dynamic works page</div>;
+};
+
+export default Work;
---
+++ components/MegaMenu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MegaMenu = () => {
+  return <div>MegaMenu</div>;
+};
